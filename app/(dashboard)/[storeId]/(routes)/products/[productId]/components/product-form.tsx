5b87394b3dfcf9895d72361c193e7491bb6334cd
tarek magdy <tarekmagdy21@outlook.com>
2023-09-04 13:36:00 +0300

    ts ignore

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -311,6 +311,8 @@ export const ProductForm: React.FC<ProductFormProps> = ({
                   <FormControl>
                     <Checkbox
                       checked={field.value}
+                      // @ts-ignore
+
                       onCheckedChange={field.onChange}
                     />
                   </FormControl>
